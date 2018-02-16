81ef45220a4a4f9c61e7fab2d64244043759c823
Pimprenelle <31759861+pimprenellep@users.noreply.github.com>
2018-02-16 13:11:10 +0100

    mises à jour - texture

--- src/simple-version/fishmodeler.js
+++ src/simple-version/fishmodeler.js
@@ -1,5 +1,5 @@
 
-const USING_TEXTURE = false;
+const USING_TEXTURE = true;
 
 const FishModeler = {
 
@@ -13,10 +13,31 @@ drawBody: function(scene, trace) {
 	geometry.applyMatrix(new THREE.Matrix4().makeRotationZ(-Math.PI/2));
 	geometry.applyMatrix(new THREE.Matrix4().makeScale(1, 1, 0.3));
 
+	geometry.uvsNeedUpdate = true;
+	var n = geometry.faceVertexUvs[0].length;
+	
+	for(var k=0; k<n; k++){
+		var face = geometry.faces[k];
+		var a = face.a;
+		var pointa = geometry.vertices[face.a];
+		var pointb = geometry.vertices[face.b];
+		var pointc = geometry.vertices[face.c];
+		geometry.faceVertexUvs[0][k][0] = new THREE.Vector2(pointa.x/100,pointa.y/100);
+		geometry.faceVertexUvs[0][k][1] = new THREE.Vector2(pointb.x/100,pointb.y/100);
+		geometry.faceVertexUvs[0][k][2] = new THREE.Vector2(pointc.x/100,pointc.y/100);
+	}
+
 	var material;
+	var loader = new THREE.TextureLoader();
+	loader.load('./../data/fish-torrique.jpg');
+	texture = new THREE.TextureLoader().load('./../data/fish-torrique.jpg');
+	texture.wrapS = THREE.RepeatWrapping;
+	texture.wrapT = THREE.RepeatWrapping;
+	texture.repeat.set( 10, 10 );
 	if(USING_TEXTURE)
+
 		material = new THREE.MeshBasicMaterial(
-			{side:THREE.DoubleSide, map: THREE.ImageUtils.loadTexture('./../data/fish-texture-rot.jpg')});
+			{side:THREE.DoubleSide, map: texture, });
 	else
 		material = new THREE.MeshNormalMaterial(
 			{side:THREE.DoubleSide});
@@ -24,11 +45,12 @@ drawBody: function(scene, trace) {
 	fish = new THREE.Mesh(geometry, material);
 	fish.name = 'fish-body';
 	scene.add(fish);
+	console.log(geometry);
 },
 
 smoothPoints: function(points){
 	const spline = new THREE.SplineCurve(points);
-	const numSamplePoints = 30;
+	const numSamplePoints = 50;
 	return spline.getPoints(numSamplePoints);
 },
 
