1cd12315a5382aa48bc9c351b8fb813c2296c502
Pedro Freire <pedrofreirex@gmail.com>
2018-03-30 03:35:07 +0200

    Add touch support

--- src/simple-version/inputhandler.js
+++ src/simple-version/inputhandler.js
@@ -45,3 +45,50 @@ function updateMousePosition(event) {
 
 document.addEventListener('mousemove', updateMousePosition);
 
+
+function touchHandler(event)
+{
+    var touches = event.changedTouches,
+        first = touches[0],
+        type = "";
+    switch(event.type)
+    {
+        case "touchstart": type = "mousedown"; break;
+        case "touchmove":  type = "mousemove"; break;        
+        case "touchend":   type = "mouseup";   break;
+        default:           return;
+	}
+	
+	if(event.type == "touchstart") {
+		Input.mouse.x = 2*(first.clientX/window.innerWidth) - 1;
+		Input.mouse.y = (-1) * (2*(first.clientY/window.innerHeight) - 1);
+	}
+
+
+    // initMouseEvent(type, canBubble, cancelable, view, clickCount, 
+    //                screenX, screenY, clientX, clientY, ctrlKey, 
+    //                altKey, shiftKey, metaKey, button, relatedTarget);
+
+    var simulatedEvent = document.createEvent("MouseEvent");
+    simulatedEvent.initMouseEvent(type, true, true, window, 1, 
+                                  first.screenX, first.screenY, 
+                                  first.clientX, first.clientY, false, 
+                                  false, false, false, 0/*left*/, null);
+
+	first.target.dispatchEvent(simulatedEvent);
+
+	if(event.type == "touchend") {
+		event.target.click();
+	}
+
+    event.preventDefault();
+}
+
+function initTouch() {
+    document.addEventListener("touchstart", touchHandler, true);
+    document.addEventListener("touchmove", touchHandler, true);
+    document.addEventListener("touchend", touchHandler, true);
+    document.addEventListener("touchcancel", touchHandler, true);    
+}
+
+initTouch();
